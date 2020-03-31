c90938944e96539f5e60cd3f340d03e04731f261
ArefianDaniel <arefianscreed@gmail.com>
2020-03-31 12:42:30 -0400

    update main.js

--- Assignment2/main.js
+++ Assignment2/main.js
@@ -5,9 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 
-document.querySelector(".thumb-bar").addEventListener("click", myFunction2);
-function myFunction2() {
-}
+
 
 
 let imgs = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
@@ -47,27 +45,4 @@ count++;
 
 }
 }
-/*
-const btntxt = btn.getAttribute('class');
 
-document.querySelector(".dark").addEventListener("click", myFunction);
-
-function myFunction() {
-
-if (btntxt === 'dark') {
-    btn.onclick = function(){
-        btn.setAttribute('class', 'light');
-        btn.textContent ='Lighten';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    };
-} 
-
-if (btntxt === 'light') {
-    
-    btn.onclick = function(){
-        btn.setAttribute('class', 'dark');
-        btn.textContent ='Darken';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-    };
-}
-} */
